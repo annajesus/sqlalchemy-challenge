219da8fa440959a7d4bf8239350e403fc567a6b0
Anna <108558769+annajesus@users.noreply.github.com>
2022-11-20 12:59:56 -0500

    save progress, update on surfs.py

---  SurfsUp/surfs_up.py
+++  SurfsUp/surfs_up.py
@@ -41,12 +41,13 @@ app = Flask(__name__)
 def home():
     return ("Surfs Up Weather API!<br><br>"
         f"Available Routes:<br/>"
-        f"/api/v1.0/precipitation <br/>"
+        f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
         f"/api/v1.0/tobs<br/>"
-        f"/api/v1.0/<start><br/>"
-        f"/api/v1.0/<start>/<end><br/>"
+        f"/api/v1.0/date<br/>"
+        f"/api/v1.0/start/end<br/>"
     )
+
 
 """Convert the query results from your precipitation analysis (i.e. retrieve only the
     last 12 months of data) to a dictionary using date as the key and prcp as the value."""
@@ -67,18 +68,20 @@ def precipitation():
 
     return jsonify(yr_prcp_results)
 
+
 """Return a JSON list of stations from the dataset."""
-@app.route("/api/v1.0/Station")
-def Station():
+@app.route("/api/v1.0/stations")
+def stations():
     # Query all station 
-    results = session.query(Station.station).all()
+    station_results = session.query(Station.station).all()
 
     session.close()
 
     # Convert list of tuples into normal list
-    all_stations = list(np.ravel(results))
+    stations = list(np.ravel(station_results))
 
-    return jsonify(all_stations)
+    return jsonify(stations = stations) 
+
 
 """Query the dates and temperature observations of the most-active station for the previous year of data."""
 @app.route("/api/v1.0/tobs")
@@ -86,15 +89,13 @@ def tobs():
     # Calculate the date one year from the last date in data set.
     past_date_one_yr = dt.date(2017, 8, 23) - dt.timedelta(days=365)
     
-    # Perform a query to retrieve the data and precipitation scores
-    prcp_scores_query = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date > past_date_one_yr).order_by(Measurement.date.desc()).all()
+    # Perform a query to retrieve the data and precipitation observations
+    prcp_scores_query = session.query(Measurement.tobs).filter(Measurement.date > past_date_one_yr).all()
 
     session.close()
-
-    last_12_months = session.query(Measurement.date, Measurement.prcp).order_by(Measurement.date.asc()).all()
     
     # Convert list of tuples into normal list
-    temperature_list = list(np.ravel(temperature_list))
+    temperature_list = list(np.ravel(prcp_scores_query))
 
 	# Jsonify summary
     return jsonify(temperature_list)
@@ -102,10 +103,10 @@ def tobs():
 
 """Return a JSON list of the minimum temperature, the average temperature, and the
     maximum temperature for a specified start or start-end range."""
-@app.route("/api/v1.0/<start>")
-def start_date(start):
+@app.route("/api/v1.0/<date>")
+def start_date(date):
     # create search for user input
-    search_date = dt.datetime.strptime(start, "%Y-%m-%d")
+    search_date = dt.datetime.strptime(date, "%Y-%m-%d")
     
     start = search_date
 
@@ -118,7 +119,6 @@ def start_date(start):
     json_start_query = list(np.ravel(start_query))
 
     return jsonify(json_start_query)
-
 
 
 @app.route("/api/v1.0/<start>/<end>")
@@ -140,6 +140,6 @@ def start_end_date(start, end):
 
     return jsonify(json_start_end_query)
 
-
 if __name__ == '__main__':
     app.run(debug=True)
+
